e48ca7d3857823589145fe545646373f814edbca
Reyioa <nicholas.simeon2020@gmail.com>
2022-04-15 14:24:06 -0400

    Added alchemite ingot, fire gold, and frozen aquamarine

---
+++ src/main/java/net/reyioa/alchemy/item/ModItems.java
@@ -0,0 +1,32 @@
+package net.reyioa.alchemy.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.reyioa.alchemy.AlchemyMod;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, AlchemyMod.MOD_ID);
+
+    public static final RegistryObject<Item> FIRE_GOLD  = ITEMS.register("fire_gold",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> ALCHEMITE_INGOT  = ITEMS.register("alchemite_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> FROZEN_AQUAMARINE  = ITEMS.register("frozen_aquamarine",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+
+
+}
